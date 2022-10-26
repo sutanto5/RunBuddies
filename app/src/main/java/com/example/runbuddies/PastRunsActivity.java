ce0b2d3fd0bed7213fb80836e8383735fc560388
thomas8238 <90787151+thomas8238@users.noreply.github.com>
2022-10-26 08:05:08 -0500

    Created activites
    all activities needed as of 10/26

---
+++ app/src/main/java/com/example/runbuddies/PastRunsActivity.java
@@ -0,0 +1,14 @@
+package com.example.runbuddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PastRunsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_runs);
+    }
+}
---
+++ app/src/main/java/com/example/runbuddies/ProfileCreator.java
@@ -0,0 +1,14 @@
+package com.example.runbuddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_creator);
+    }
+}
