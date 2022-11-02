f24ac8028c54d26fd2ed66af145f22255a357ac1
nance1798 <nance1798@students.d211.org>
2022-11-02 08:11:54 -0500

    did sign up activity and started profile creator

--- app/src/main/java/com/example/runbuddies/ProfileCreator.java
+++ app/src/main/java/com/example/runbuddies/ProfileCreator.java
@@ -3,8 +3,20 @@ package com.example.runbuddies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class ProfileCreator extends AppCompatActivity {
+public class ProfileCreator extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner;
+    EditText cityET;
+    EditText stateET;
+    EditText bioET;
+    Button 
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
