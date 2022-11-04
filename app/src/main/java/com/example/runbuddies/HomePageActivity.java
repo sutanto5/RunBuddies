10551a1eb8732ad3d959eeb98f2e2fb543fb71d3
thomas8238 <90787151+thomas8238@users.noreply.github.com>
2022-11-04 07:56:36 -0500

    edited homepages

--- app/src/main/java/com/example/runbuddies/HomePageActivity.java
+++ app/src/main/java/com/example/runbuddies/HomePageActivity.java
@@ -2,7 +2,10 @@ package com.example.runbuddies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+    public void logOutClicked(View view) {
+        LogInActivity.firebaseHelper.logOutUser();
+        Intent intent = new Intent(HomePageActivity.this, LogInActivity.class);
+        startActivity(intent);
     }
 }
