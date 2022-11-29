e86bd60150b2e7c716ed560f0d9b75f314d03ce7
sutanto7475 <76191538+sutanto7475@users.noreply.github.com>
2022-11-29 08:21:59 -0600

    Run Stuff

--- app/src/main/java/com/example/runbuddies/SaveRun.java
+++ app/src/main/java/com/example/runbuddies/SaveRun.java
@@ -16,11 +16,18 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
@@ -38,6 +45,7 @@ import java.util.Locale;
 // 7 - Stop updating locations if not needed to save battery
 
 public class SaveRun extends AppCompatActivity implements LocationListener {
+    DecimalFormat decimalFormat = new DecimalFormat(".##");
 
     float x1,x2;
 
@@ -46,6 +54,23 @@ public class SaveRun extends AppCompatActivity implements LocationListener {
     private LocationManager locationManager;
     private Location last;
     private long distance;
+    private int seconds = 0;
+    private double pace = 0;
+
+    // Is the stopwatch running?
+    private boolean running;
+    private boolean wasRunning;
+
+    TextView runDistance;
+    TextView runTime;
+    TextView runPace;
+    Button start;
+    Button stop;
+    Button save;
+    Button reset;
+    TextView distanceTV;
+    TextView paceTV;
+    TextView timeTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,12 +83,212 @@ public class SaveRun extends AppCompatActivity implements LocationListener {
         address = findViewById(R.id.address);
         distanceView = findViewById(R.id.distance);
 
+        seconds
+                = savedInstanceState
+                .getInt("seconds");
+        running
+                = savedInstanceState
+                .getBoolean("running");
+        wasRunning
+                = savedInstanceState
+                .getBoolean("wasRunning");
+
 
         //first check and ask permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             //checks for permission
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
         }
+
+        runTimer();
+        runTime = findViewById(R.id.time_view);
+        runPace = findViewById(R.id.pace_View);
+        runDistance = findViewById(R.id.milesView);
+        start = findViewById(R.id.startButton);
+        stop = findViewById(R.id.stopButton);
+        save = findViewById(R.id.save);
+        reset = findViewById(R.id.reset);
+        distanceTV = findViewById(R.id.distanceTV);
+        paceTV = findViewById(R.id.paceTV);
+        timeTV = findViewById(R.id.TIMETV);
+    }
+
+    // Save the state of the stopwatch
+    // if it's about to be destroyed.
+    @Override
+    public void onSaveInstanceState(
+            Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState
+                .putInt("seconds", seconds);
+        savedInstanceState
+                .putBoolean("running", running);
+        savedInstanceState
+                .putBoolean("wasRunning", wasRunning);
+    }
+
+    // If the activity is paused,
+    // stop the stopwatch.
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    // If the activity is resumed,
+    // start the stopwatch
+    // again if it was running previously.
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    // Start the stopwatch running
+    // when the Start button is clicked.
+    // Below method gets called
+    // when the Start button is clicked.
+    public void onClickStart(View view)
+    {
+        running = true;
+        start.setVisibility(View.GONE);
+        start.setText("Resume");
+        reset.setVisibility(View.INVISIBLE);
+        save.setVisibility(View.INVISIBLE);
+        stop.setVisibility(View.VISIBLE);
+    }
+
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 0;
+        pace = 0;
+        distance= 0;
+        Intent intent = new Intent(SaveRun.this,HomePageActivity.class);
+        startActivity(intent);
+    }
+
+    public void addRun(View view) {
+        ArrayList<View> views = new ArrayList<View>(
+                Arrays.asList(start,stop,save,reset,distanceTV,paceTV,timeTV,runDistance,runTime,runPace));
+        View nameTV = findViewById(R.id.nameTV);
+        EditText name = findViewById(R.id.runNameEditText);
+        Button upload = findViewById(R.id.upload);
+        for(View v:views) {
+            v.setVisibility(View.GONE);
+        }
+        name.setVisibility(View.VISIBLE);
+        nameTV.setVisibility(View.VISIBLE);
+        upload.setVisibility(View.VISIBLE);
+    }
+    public void addRunButtonClicked(View view) {
+        EditText nameET = findViewById(R.id.runNameEditText);
+        String name = nameET.getText().toString();
+        String dist = runDistance.getText().toString();
+        String time = runTime.getText().toString();
+        String pace = runPace.getText().toString();
+        Calendar c = Calendar.getInstance();
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH);
+        int year = c.get(Calendar.YEAR);
+        String date = (month+1) + "/" + (day) + "/" + year;
+        Run r = new Run(date, dist, pace, time,name);
+
+
+        LogInActivity.firebaseHelper.addRunData(r);
+        onClickReset(view);
+    }
+
+    // Sets the NUmber of seconds on the timer.
+    // The runTimer() method uses a Handler
+    // to increment the seconds and
+    // update the text view.
+    private void runTimer()
+    {
+
+        // Get the text view.
+        final TextView timeView
+                = findViewById(
+                R.id.time_view);
+        final TextView paceView
+                = findViewById(
+                R.id.pace_View);
+        final TextView distView
+                = findViewById(
+                R.id.milesView);
+        // Creates a new Handler
+        final Handler handler
+                = new Handler();
+
+        // Call the post() method,
+        // passing in a new Runnable.
+        // The post() method processes
+        // code without a delay,
+        // so the code in the Runnable
+        // will run almost immediately.
+        handler.post(new Runnable() {
+            @Override
+
+            public void run()
+            {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                pace = (int)(seconds /distance);
+                double paceHours = pace/ 3600;
+                double paceMinutes = (pace % 3600) / 60;
+                double paceSecs = pace % 60;
+
+                // Format the seconds into hours, minutes,
+                // and seconds.
+                String time;
+                if(hours >0) {
+                    time = String
+                            .format(Locale.getDefault(),
+                                    "%d:%02d:%02d", hours,
+                                    minutes, secs);
+                } else {
+                    time = String
+                            .format(Locale.getDefault(),
+                                    "%02d:%02d",
+                                    minutes, secs);
+                }
+                String pace;
+                if(paceHours >0) {
+                    pace = String
+                            .format(Locale.getDefault(),
+                                    "%d:%02d:%02d", paceHours,
+                                    paceMinutes, paceSecs);
+                } else {
+                    pace = String
+                            .format(Locale.getDefault(),
+                                    "%02d:%02d",
+                                    paceMinutes, paceSecs);
+                }
+                pace +="/mi";
+
+                // Set the text view text.
+                timeView.setText(time);
+                paceView.setText(pace);
+                distView.setText(decimalFormat.format(distance) + "mi");
+
+
+                // If running is true, increment the
+                // seconds variable.
+                if (running) {
+                    seconds++;
+                }
+
+                // Post the code again
+                // with a delay of 1 second.
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 
     // @Override
@@ -155,13 +380,6 @@ public class SaveRun extends AppCompatActivity implements LocationListener {
         locationManager.removeUpdates(this);
     }
 
-
-
-
-
-
-
-
     @Override
     public void onLocationChanged(@NonNull List<Location> locations) {
         LocationListener.super.onLocationChanged(locations);
@@ -185,24 +403,6 @@ public class SaveRun extends AppCompatActivity implements LocationListener {
     @Override
     public void onProviderDisabled(@NonNull String provider) {
         LocationListener.super.onProviderDisabled(provider);
-    }
-    //i.e. Haversine formula
-    //https://community.esri.com/t5/coordinate-reference-systems-blog/distance-on-a-sphere-the-haversine-formula/ba-p/902128
-    public double getDistance(double startLatitude, double startLongitude, double curLatitude, double curLongitude){
-        //Earth' radius in meters
-        double R = 6371000;
-
-        double deltaLat = Math.toRadians(startLatitude - curLatitude);
-        double deltaLong = Math.toRadians(startLongitude - curLongitude);
-
-        double a = Math.pow(Math.sin(deltaLat/2),2) + Math.cos(Math.toRadians(startLatitude)) *Math.cos(Math.toRadians(curLatitude))
-                * Math.pow(Math.sin(deltaLong/2), 2);
-
-        double b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        double c = R * b;
-
-        return c;
-
     }
 
     public boolean onTouchEvent(MotionEvent touchEvent){
