069e2a72022e0519523919fcfea22b22ed53f423
JonMoyy <90794057+JonMoyy@users.noreply.github.com>
2022-12-05 16:52:40 -0600

    Edit Profile

--- app/src/main/java/com/example/runbuddies/EditProfileActivity.java
+++ app/src/main/java/com/example/runbuddies/EditProfileActivity.java
@@ -4,26 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class EditProfileActivity extends AppCompatActivity {
 
 
-    public final String TAG = "Denna";
+    public final String TAG = "MOY";
     EditText nameET, bioET, stateET, cityET;
-
+    Profile userProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+        nameET = findViewById(R.id.nameEditText);
+        bioET = findViewById(R.id.bioEditText);
+        stateET = findViewById(R.id.stateEditText);
+        cityET = findViewById(R.id.cityEditText);
+
+        Intent intent = getIntent();
+
+        /**
+        userProfile = intent.getParcelableExtra(Profile);
+         */
 
         nameET = findViewById(R.id.nameEditText);
         bioET = findViewById(R.id.bioEditText);
         stateET = findViewById(R.id.stateEditText);
         cityET = findViewById(R.id.cityEditText);
+
+    }
+
+
+
+    public void saveMemoryEdits(View v) {
+        Log.d(TAG, "inside saveMemoryEdits method");
+        // updates the currentMemory object to have the same name/desc that are on the screen
+        // in the event of changes made.
+        userProfile.setName(nameET.getText().toString());
+        userProfile.setBio(bioET.getText().toString());
+
+        // Calls editData with this updated Memory object
+
+        /**
+        LogInActivity.firebaseHelper.editData(userProfile);
+         */
     }
 
 
